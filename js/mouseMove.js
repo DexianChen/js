08d050500a3ac09b5515bc7381a53995b1207bca
DexianChen <dex2015@aliyun.com>
2019-06-28 23:30:15 +0800

    fix():鼠标移入移出效果 feat():添加README.txt文件

--- js/mouseMove.js
+++ js/mouseMove.js
@@ -1,12 +1,13 @@
 $(function () {
 	//mousemove([[data],fn]),mouseout([[data],fn]) 两者一般一起使用
+	var background = $("#studentTable tbody tr").css("color");
 	$("#studentTable tbody tr").mouseover(function () {
 		//css(name|pro|[,val|fn]), 其中pro取properties, 要设置为样式属性的名/键值对
 		//改变字体颜色,红色
 		$(this).css({"color":"#ff0011"});
 	}).mouseout(function () {
 		//黑色
-		$(this).css({"color":"#000000"});
+		$(this).css({"color":background});
 	});
 });
 
