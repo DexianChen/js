df1ce16292ac44ff312aa9b9ded0eef944283d55
chendexian <4>
2019-07-02 17:47:25 +0800

    fix(): 完成-章-的分析

--- js/book_view.js
+++ js/book_view.js
@@ -1,5 +1,9 @@
 $(function () {
-    // alert(returnObjectInfo($("#book_page div:not('[id]')").find("p")));
+    // 章、节、条文列表
+    var chapterList = [];
+    var nodeList = [];
+    var itemList = [];
+
     var array = $("#book_page div:not('[id]')").find("p").toArray();
     $.each(array, function (index, value) {
         var html = $(value).html();
@@ -7,12 +11,24 @@ $(function () {
         //以数字开头的正则表达式
         var textReg = /^\d/;
         if (textReg.test(html)) {
-           // 判断是否含有<br>
-            if (html.indexOf("<br>") == -1 && html.indexOf("\\.") == -1) {
-                    alert(html);
+           // 判断是否含有<br>,有些章带着节，中间有<br>
+            if (html.indexOf("<br>") == -1) {
+                // ．非句号，从页面拷贝过来，不含该符号的即为章
+                if (html.indexOf("．") == -1) {
+                    // 这里不能仅仅获取个位数的  TODO
+                    var chapter = {"id":html.substring(0,html.indexOf(" ")), "content":html.substring(html.indexOf(" ")).trim()};
+                    // alert(chapter.content)
+                    chapterList.push(chapter);
+                }
             }else {
-                alert(html.substring(0, html.indexOf("<br>")));
+                //带着节的章，则截取
+                var chapter = {"id":html.substring(0,html.indexOf(" ")), "content":html.substring(html.indexOf(" "), html.indexOf("<br>")).trim()};
+                chapterList.push(chapter);
             }
         }
-    })
+    });
+
+    $.each(chapterList, function (index, value) {
+        alert(value.id + "   " + value.content);
+    });
 });
